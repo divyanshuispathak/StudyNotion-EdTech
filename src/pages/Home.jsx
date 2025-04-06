d8b269411bf5920cc9f0de4e53887039499c9d1f
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-04-06 15:38:54 +0530

    resolved vite config related issues and created structure for home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+    return (
+        <div>
+            {/* {Section 1} */}
+            <div className="relative mx-auto flex flex-col w-11/12 items-center text-white">
+                <Link to="/signup">
+                    <div>
+                        <div>
+                            <p>Become an Instructor</p>
+                            <FaArrowRight />
+                        </div>
+                    </div>
+                </Link>
+
+
+            </div>
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Home;
