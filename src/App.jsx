af862404ae0365129b08486495e2beb1c3d5b6e5
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-05-06 13:10:22 +0530

    created complete flow of signup, login, forgot password, verify email, send otp and tested all its functionality related to it

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import OpenRoute from "./components/Core/Auth/OpenRoute";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
 
 function App() {
   return (
@@ -25,6 +28,30 @@ function App() {
           element={
             <OpenRoute>
               <Login />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
             </OpenRoute>
           }
         />
