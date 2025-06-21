829589e290445e80f46e4006c632ff7de24af7b8
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-06-21 18:41:38 +0530

    added css to about page and its children components

--- src/components/Core/AboutPage/ContactFormSection.jsx
+++ src/components/Core/AboutPage/ContactFormSection.jsx
@@ -4,9 +4,11 @@ import ContactUsForm from "../../ContactPage/ContactUsForm";
 const ContactFormSection = () => {
   return (
     <div className="mx-auto">
-      <h1>Get in Touch</h1>
-      <p>We'd love to here from you, Please fill out this form.</p>
-      <div>
+      <h1 className="text-center text-4xl font-semibold">Get in Touch</h1>
+      <p className="text-center text-richblack-300 mt-3">
+        We&apos;d love to here for you, Please fill out this form.
+      </p>
+      <div className="mt-12 mx-auto">
         <ContactUsForm />
       </div>
     </div>
